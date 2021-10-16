d43df561605d54bdc7f5d9eaefe9a56f8d795b19
RizlaneAbalil <38002332@parisnanterre.fr>
2021-10-16 23:42:24 +0200

    TD1 - update enum 'MeatType'

--- td1-students/src/main/java/td1/original/refractor/api/general/MeatType.java
+++ td1-students/src/main/java/td1/original/refractor/api/general/MeatType.java
@@ -1,5 +1,36 @@
 package main.java.td1.original.refractor.api.general;
 
-public enum MeatType {
-    MEAT_MENU, FISH_MENU, CHEESE_MENU ;
+public enum MeatType implements FoodConstituent{
+    BEEF, WHITEFISH;
+
+    @Override
+    public double calories_per_100g()
+    {
+        double rtn;
+        switch(this)
+        {
+            case BEEF :
+                rtn = 200;
+                break;
+            default:
+                rtn = 170;
+                break;
+        }
+        return rtn;
+    }
+
+    public double price()
+    {
+        double rtn;
+        switch(this)
+        {
+            case BEEF :
+                rtn = 4;
+                break;
+            default:
+                rtn = 6;
+                break;
+        }
+        return rtn;
+    }
 }
