a05fbed752c5f381b406b3275e57c027bafa62f7
RizlaneAbalil <38002332@parisnanterre.fr>
2021-10-16 17:39:37 +0200

    TD1 - add enum 'SauceType'

--- td1-students/src/main/java/td1/original/refractor/api/general/SauceType.java
+++ td1-students/src/main/java/td1/original/refractor/api/general/SauceType.java
@@ -1,4 +1,30 @@
 package main.java.td1.original.refractor.api.general;
 
-public enum SauceType {
+public enum SauceType implements FoodConstituent{
+    BURGER, BARBECUE, BEARNAISE;
+
+    // BURGER : 240 kcal / 100g
+    // BARBECUE : 130 kcal / 100g
+    // BEARNAISE : 550 kcal / 100g
+
+    public double calories_per_100g()
+    {
+        double rtn;
+        switch (this)
+        {
+            case BURGER -> {
+                rtn = 240;
+                break;
+            }
+            case BARBECUE -> {
+                rtn = 130;
+                break;
+            }
+            default -> {
+                rtn = 550;
+                break;
+            }
+        }
+        return rtn;
+    }
 }
