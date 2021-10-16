00e4bbe6e36330fd77ae41d882bb0d99122a9f75
RizlaneAbalil <38002332@parisnanterre.fr>
2021-10-17 00:36:16 +0200

    TD1 - add class 'MacDeauRestaurant'

--- td1-students/src/main/java/td1/original/refractor/api/macdeau/MacDeauRestaurant.java
+++ td1-students/src/main/java/td1/original/refractor/api/macdeau/MacDeauRestaurant.java
@@ -5,14 +5,18 @@ import main.java.td1.original.refractor.api.general.MeatType;
 import main.java.td1.original.refractor.api.general.MenuType;
 import main.java.td1.original.refractor.api.general.Size;
 
+import static main.java.td1.original.refractor.api.general.MeatType.BEEF;
+import static main.java.td1.original.refractor.api.general.MeatType.WHITEFISH;
+import static main.java.td1.original.refractor.api.general.Size.MEDIUM;
+import static main.java.td1.original.refractor.api.general.Size.SMALL;
+
 public class MacDeauRestaurant {
 
     private static MacDeauRestaurant instance;
 
     //Constructeur interne au package
-    MacDeauRestaurant()
+    private MacDeauRestaurant()
     {
-        //this.instance ?
     }
 
     MacDeauRestaurant queue()
@@ -23,12 +27,25 @@ public class MacDeauRestaurant {
 
     BurgerCook order_menu(MenuType menu)
     {
-        BurgerCook
+        BurgerCook burgerCook;
+        switch (menu)
+        {
+            case MEAT_MENU:
+                burgerCook = new MacDeauCook("Beefy", MEDIUM, BEEF);
+                break;
+            case FISH_MENU:
+                burgerCook = new MacDeauCook("Fishy", SMALL, WHITEFISH);
+                break;
+            default:
+                burgerCook = new MacDeauCook("Cheesy", SMALL, BEEF);
+                break;
+        }
+        return burgerCook;
     }
 
     BurgerCook order_personal(Size size, MeatType type)
     {
-        BurgerCook burgerCook = new BurgerCook(size, type);
+        BurgerCook burgerCook = new MacDeauCook("Own style", size, type);
         return burgerCook;
     }
 }
