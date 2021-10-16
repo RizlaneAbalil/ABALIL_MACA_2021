96673950ee03dc5b98476919af78e163c80717e0
RizlaneAbalil <38002332@parisnanterre.fr>
2021-10-16 16:51:44 +0200

    TD1 - update interface 'BurgerCook'

--- td1-students/src/main/java/td1/original/refractor/api/general/BurgerCook.java
+++ td1-students/src/main/java/td1/original/refractor/api/general/BurgerCook.java
@@ -1,4 +1,10 @@
 package main.java.td1.original.refractor.api.general;
 
 public interface BurgerCook {
+
+    public BurgerCook with_cheese();
+    public BurgerCook with_onions();
+    public BurgerCook with_sauce(SauceType sauce);
+    public BurgerCook with_tomato();
+    public Burger cook();
 }
