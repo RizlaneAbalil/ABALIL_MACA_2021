4306e8f52e5cb864b6344d2835819c020d378cb2
RizlaneAbalil <38002332@parisnanterre.fr>
2021-10-17 23:56:54 +0200

    TD1 - add interface 'BurgerRestaurant'

--- td1-students/src/main/java/td1/original/refractor/api/general/BurgerRestaurant.java
+++ td1-students/src/main/java/td1/original/refractor/api/general/BurgerRestaurant.java
@@ -1,4 +1,8 @@
 package main.java.td1.original.refractor.api.general;
 
 public interface BurgerRestaurant {
+
+    public BurgerCook order_menu(MenuType type);
+
+    public BurgerCook order_personal(Size size, MeatType type);
 }
