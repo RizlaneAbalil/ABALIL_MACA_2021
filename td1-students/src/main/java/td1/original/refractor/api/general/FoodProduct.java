890264ca4a28d8724228c14fdd69126adcd1bb5a
RizlaneAbalil <38002332@parisnanterre.fr>
2021-10-18 00:07:15 +0200

    TD1 - add interface 'FoodProduct'

--- td1-students/src/main/java/td1/original/refractor/api/general/FoodProduct.java
+++ td1-students/src/main/java/td1/original/refractor/api/general/FoodProduct.java
@@ -1,4 +1,9 @@
 package main.java.td1.original.refractor.api.general;
 
-public interface FoodProduct {
+public interface FoodProduct extends FoodConstituent, Product{
+
+    public default double calories()
+    {
+        return weight() / 100 * calories_per_100g();
+    }
 }
