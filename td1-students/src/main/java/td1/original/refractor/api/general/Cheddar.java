c5e3f46aaf8fab472f0663e63e395d99fdc286fe
RizlaneAbalil <38002332@parisnanterre.fr>
2021-10-18 00:33:39 +0200

    TD1 - update class 'Cheddar'

--- td1-students/src/main/java/td1/original/refractor/api/general/Cheddar.java
+++ td1-students/src/main/java/td1/original/refractor/api/general/Cheddar.java
@@ -1,10 +1,10 @@
 package main.java.td1.original.refractor.api.general;
 
-public class Cheddar implements Product {
+public class Cheddar implements FoodProduct {
     // 400 kcal / 100g
 
     private double weight;
-    private double BASE_PRICE = 4;
+    private static double BASE_PRICE = 4;
 
     public Cheddar(double weight) {
         this.weight = weight;
@@ -23,5 +23,10 @@ public class Cheddar implements Product {
     @Override
     public String toString() {
         return String.format("cheddar (%.0fg) -- %.2f€", weight(), price());
+    }
+
+    @Override
+    public double calories_per_100g() {
+        return 400;
     }
 }
