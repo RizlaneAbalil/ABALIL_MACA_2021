5498c2f8d1e5ddfdfdabbd9d4fa23a0048b499aa
RizlaneAbalil <38002332@parisnanterre.fr>
2021-10-19 11:01:28 +0200

    TD2 - creation class 'Fichier'

---
+++ TD2/app/src/main/java/td2/filesystem/Fichier.java
@@ -0,0 +1,36 @@
+package main.java.td2.filesystem;
+
+public class Fichier extends ComposantImpl implements Composant{
+
+    private StringBuilder content;
+
+    public Fichier(String name, Owner owner)
+    {
+        super(name, owner);
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public String getContent() {
+        return this.content.toString();
+    }
+
+    @Override
+    public void appendContent(String name) {
+        this.content.append(name);
+    }
+
+    @Override
+    public boolean isComposite() {
+        return false;
+    }
+
+    public String toString()
+    {
+        return this.getContent();
+    }
+}
