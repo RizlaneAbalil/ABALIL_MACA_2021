4b9e0a31c2ae45c974ae8d906ed42b924f48600e
RizlaneAbalil <38002332@parisnanterre.fr>
2021-10-19 18:54:56 +0200

    TD2 - forumv2 : update interface 'IConteneur'

--- TD2/app/src/main/java/td2/forum/v2/IConteneur.java
+++ TD2/app/src/main/java/td2/forum/v2/IConteneur.java
@@ -1,6 +1,6 @@
 package main.java.td2.forum.v2;
 
-public interface IConteneur {
+public interface IConteneur extends Composant {
     public void add(T child);
     public void remove(int index);
     public int size();
