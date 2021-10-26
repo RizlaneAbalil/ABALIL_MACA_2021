8e0b0a82d90070bfd0b185b55c96706ab6c27096
RizlaneAbalil <38002332@parisnanterre.fr>
2021-10-26 11:06:19 +0200

    TD3 - completing class 'GroupeClient'

--- TD3/app/src/main/java/TD3/commandes/GroupeClient.java
+++ TD3/app/src/main/java/TD3/commandes/GroupeClient.java
@@ -5,14 +5,42 @@ import TD3.visitable.Visitable;
 import TD3.visitors.PrePostVisitor;
 import TD3.visitors.Visitor;
 
+import java.util.List;
+
 public class GroupeClient implements PrePostVisitable, Visitable {
+    private String name;
+    private List<Client> clients ;
+
+    public GroupeClient(String name)
+    {
+        this.name = name;
+    }
+
+    public void addClient(Client client)
+    {
+        this.clients.add(client);
+    }
+
+    public void addCommande(String name, Commande commande)
+    {
+        this.name=name;
+        this.clients.addCommande(commande);
+    }
+
+    public void addLigne(String name, String nameL, Ligne ligne)
+    {
+        this.name = name;
+        this.clients.addLigne(nameL, ligne);
+    }
+
     @Override
     public void accept(PrePostVisitor prePostVisitor) {
-
+        prePostVisitor.preVisit(this);
+        prePostVisitor.postVisit(this);
     }
 
     @Override
     public void accept(Visitor visitor) {
-
+        visitor.visit(this);
     }
 }
