57937f9862b38764c59cbf60acb89006dddd8025
RizlaneAbalil <38002332@parisnanterre.fr>
2021-10-26 11:12:42 +0200

    TD3 - completing class 'Commande'

--- TD3/app/src/main/java/TD3/commandes/Commande.java
+++ TD3/app/src/main/java/TD3/commandes/Commande.java
@@ -5,14 +5,30 @@ import TD3.visitable.Visitable;
 import TD3.visitors.PrePostVisitor;
 import TD3.visitors.Visitor;
 
+import java.util.List;
+
 public class Commande implements PrePostVisitable, Visitable {
+    private String name;
+    List<Ligne> lignes;
+
+    public Commande(String name)
+    {
+        this.name=name;
+    }
+
+    public void addLigne(Ligne ligne)
+    {
+        lignes.add(ligne);
+    }
+
     @Override
     public void accept(PrePostVisitor prePostVisitor) {
-
+        prePostVisitor.preVisit(this);
+        prePostVisitor.postVisit(this);
     }
 
     @Override
     public void accept(Visitor visitor) {
-
+        visitor.visit(this);
     }
 }
