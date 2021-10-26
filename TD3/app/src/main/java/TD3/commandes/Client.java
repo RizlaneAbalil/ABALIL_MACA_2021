0bba7739cf96f4f8b273374d9a8770822dda25f4
RizlaneAbalil <38002332@parisnanterre.fr>
2021-10-26 11:23:12 +0200

    TD3 - completing class 'Client'

--- TD3/app/src/main/java/TD3/commandes/Client.java
+++ TD3/app/src/main/java/TD3/commandes/Client.java
@@ -5,14 +5,37 @@ import TD3.visitable.Visitable;
 import TD3.visitors.PrePostVisitor;
 import TD3.visitors.Visitor;
 
+import java.util.Map;
+
+
 public class Client implements PrePostVisitable, Visitable {
+    private String name;
+    private Map<String, Commande> commandes;
+
+    public Client(String name)
+    {
+        this.name=name;
+    }
+
+    public void addCommande(Commande commande)
+    {
+        this.commandes.put(commande.getName(), commande);
+    }
+
+    public void addLigne(String name, Ligne l)
+    {
+        Commande tmp = this.commandes.get(name);
+        tmp.addLigne(l);
+    }
+
     @Override
     public void accept(PrePostVisitor prePostVisitor) {
-
+        prePostVisitor.preVisit(this);
+        prePostVisitor.postVisit(this);
     }
 
     @Override
     public void accept(Visitor visitor) {
-
+        visitor.visit(this);
     }
 }
