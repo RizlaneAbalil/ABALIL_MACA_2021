fd95e905b5e71fe8d1dbc21965d478e454c3ee8e
RizlaneAbalil <38002332@parisnanterre.fr>
2021-10-26 10:37:15 +0200

    TD2 - Exercice 2

---
+++ TD2/app/src/main/java/td2/forum/v2/Forum.java
@@ -0,0 +1,41 @@
+package main.java.td2.forum.v2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Forum {
+    private final String topic;
+    private List<Theme> children;
+
+    public Forum(String topic)
+    {
+        this.topic = topic;
+        this.children = new ArrayList<>();
+    }
+
+    public String sujet()
+    {
+        return this.topic;
+    }
+
+    public void add(Theme child)
+    {
+        this.children.add(child);
+    }
+
+    public void remove(int index)
+    {
+        this.children.remove(index);
+    }
+
+    public int size()
+    {
+        return this.children.size();
+    }
+
+    public String toString()
+    {
+        String str = topic;
+        return str;
+    }
+}
---
+++ TD2/app/src/main/java/td2/forum/v2/Salon.java
@@ -0,0 +1,41 @@
+package main.java.td2.forum.v2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Salon {
+    private final String topic;
+    private List<Message> children;
+
+    public Salon(String topic)
+    {
+        this.topic = topic;
+        this.children = new ArrayList<>();
+    }
+
+    public String sujet()
+    {
+        return this.topic;
+    }
+
+    public void add(Message child)
+    {
+        this.children.add(child);
+    }
+
+    public void remove(int index)
+    {
+        this.children.remove(index);
+    }
+
+    public int size()
+    {
+        return this.children.size();
+    }
+
+    public String toString()
+    {
+        String str = topic;
+        return str;
+    }
+}
