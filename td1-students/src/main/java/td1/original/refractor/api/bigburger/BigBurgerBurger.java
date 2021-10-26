3726eede01b80bd847468932ff24b8e0c9ac2660
RizlaneAbalil <38002332@parisnanterre.fr>
2021-10-26 10:37:33 +0200

    TD1

--- td1-students/src/main/java/td1/original/refractor/api/bigburger/BigBurgerBurger.java
+++ td1-students/src/main/java/td1/original/refractor/api/bigburger/BigBurgerBurger.java
@@ -1,4 +1,14 @@
 package main.java.td1.original.refractor.api.bigburger;
 
-public class BigBurgerBurger {
+import main.java.td1.original.api.general.Burger;
+import main.java.td1.original.refractor.api.general.FoodProduct;
+
+import java.awt.*;
+
+
+public class BigBurgerBurger extends Burger {
+
+    public BigBurgerBurger(String name, List<FoodProduct> items) {
+        super(name, items);
+    }
 }
--- td1-students/src/main/java/td1/original/refractor/api/general/BurgerAdvisor.java
+++ td1-students/src/main/java/td1/original/refractor/api/general/BurgerAdvisor.java
@@ -2,4 +2,6 @@ package main.java.td1.original.refractor.api.general;
 
 public class BurgerAdvisor {
 
+    public static BurgerAdvisor instance() {
+    }
 }
