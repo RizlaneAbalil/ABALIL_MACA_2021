c7bbe4bf71380ddddb1f2e138aa2a6ee434b029b
RizlaneAbalil <38002332@parisnanterre.fr>
2021-10-26 11:00:08 +0200

    TD3 - completing class 'Ligne'

--- TD3/app/src/main/java/TD3/commandes/Ligne.java
+++ TD3/app/src/main/java/TD3/commandes/Ligne.java
@@ -6,13 +6,28 @@ import TD3.visitors.PrePostVisitor;
 import TD3.visitors.Visitor;
 
 public class Ligne implements PrePostVisitable, Visitable {
+    private String name;
+    private int sum;
+
+    public Ligne(String name, int sum)
+    {
+        this.name = name;
+        this.sum = sum;
+    }
+
+    public Ligne(String name)
+    {
+        this.name = name;
+    }
+
     @Override
     public void accept(PrePostVisitor prePostVisitor) {
-
+        prePostVisitor.preVisit(this);
+        prePostVisitor.postVisit(this);
     }
 
     @Override
     public void accept(Visitor visitor) {
-
+        visitor.visit(this);
     }
 }
