a84a74b63cc96cf510490272c5aaa5a21ae1b7cc
RizlaneAbalil <38002332@parisnanterre.fr>
2021-11-16 10:20:18 +0100

    TD4 - Exercice 1 - class Context

---
+++ TD4/app/src/main/java/TD4/Allumee.java
@@ -0,0 +1,23 @@
+package TD4;
+
+public class Allumee extends AmpouleEtat{
+
+    public Allumee(Ampoule context)
+    {
+        super(context);
+    }
+    @Override
+    public void allumer(Ampoule a) {
+        //GARDE
+    }
+
+    @Override
+    public void eteindre(Ampoule a) {
+
+    }
+
+    @Override
+    public void cassee(Ampoule a) {
+
+    }
+}
